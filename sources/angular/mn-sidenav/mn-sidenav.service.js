c2cbcad65369ca389a191c3082d9394448b911dd
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-26 13:49:50 -0200

    add methods open and close to service

--- sources/angular/mn-sidenav/mn-sidenav.service.js
+++ sources/angular/mn-sidenav/mn-sidenav.service.js
@@ -1,7 +1,23 @@
 angular
-  .module('mn-input')
+  .module('mn-sidenav')
   .service('$mnSidenav', $mnSidenav)
 
 function $mnSidenav() {
-  console.log('available')
+  this.open = open
+  this.close = close
+
+  function open(id) {
+    const sidenav = document.querySelector(`mn-sidenav#${id}`)
+    console.log(sidenav)
+    if (sidenav) {
+      sidenav.open()
+    }
+  }
+
+  function close() {
+    const sidenav = document.querySelector('mn-sidenav.visible')
+    if (sidenav) {
+      sidenav.close()
+    }
+  }
 }
